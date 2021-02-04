43a5860554cab004e9159d14009f4a6b6960ac35
Spider149 <lehieu.149@gmail.com>
2021-02-04 20:53:22 +0700

    responsive, fix autoplay, autoplayhover bug

--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
         hljs.highlightBlock(block);
     });
 
+    // responsive
+    let menuButton=document.querySelector(".menu__button");
+    let menu=document.querySelector(".header__nav");
+    let body=document.getElementsByTagName("body")[0];
+
+    menuButton.addEventListener("click",function(){
+        menuButton.classList.toggle("open");
+        menu.classList.toggle("open");
+        body.classList.toggle("no-scroll");
+    })
+
+    window.addEventListener("click",function(event){
+        if(!event.target.classList.contains("header__nav") && menu.classList.contains("open") 
+        && !event.target.classList.contains("menu__button") && !event.target.parentNode.classList.contains("menu__button")) {
+          menu.classList.toggle("open");
+          menuButton.classList.toggle("open");
+          body.classList.toggle("no-scroll");
+        }
+    })
+
+    // ----- //
     document.querySelector(".own-1").ownCarousel({
         itemPerRow: 3,
         itemWidth: 32,
